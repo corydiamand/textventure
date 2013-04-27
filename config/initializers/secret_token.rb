06e604581d43d4a03c43c46d3815933ad0022380
Cory <corydiamand@corydiamand-K55N.(none)>
2013-04-27 00:55:04 -0400

    Turning this bad boy into a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textventure::Application.config.secret_token = 'fd730d60854bd736261f80a7728b03bc96f09ede4edb2601920e7486e74f9bdded1d6de658e1e037f329a11e7e87e6f99ac30887547cde3f125a265227e32fc1'
